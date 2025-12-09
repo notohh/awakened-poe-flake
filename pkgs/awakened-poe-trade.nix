efa0811f632dc49818b2befe9b70a08fb27f089c
notohh <github@notohh.dev>
2025-12-09 08:46:54 -0500

    pkgs: add desktop items

--- pkgs/awakened-poe-trade.nix
+++ pkgs/awakened-poe-trade.nix
@@ -3,18 +3,36 @@
   lib,
   fetchurl,
   appimageTools,
+  makeDesktopItem,
   ...
 }:
-
-#TODO add desktop entry etc
-
-appimageTools.wrapType2 rec {
+let
   pname = "awakened-poe-trade";
   version = "3.27.106";
+  icon = fetchurl {
+    url = "https://web.poecdn.com/image/Art/2DItems/Currency/TransferOrb.png";
+    sha256 = "sha256-DR6kzgwNefz4Va11QNP2GuieQss6OW6q74GBDwOUaD0=";
+  };
+
+  desktopItem = makeDesktopItem {
+    desktopName = "Awakened PoE Trade";
+    name = pname;
+    exec = pname;
+    icon = icon;
+  };
+in
+
+appimageTools.wrapType2 {
+  inherit pname version;
   src = fetchurl {
     url = "https://github.com/SnosMe/awakened-poe-trade/releases/download/v${version}/Awakened-PoE-Trade-${version}.AppImage";
     hash = "sha256-8L5Szn0KYfUMaTe+yyhJV1YZspmJCSlXSHXLPoiRhjE=";
   };
+
+  extraInstallCommands = ''
+    mkdir -p $out/share/applications
+    ln -s ${desktopItem}/share/applications/* $out/share/applications/
+  '';
 
   meta = with lib; {
     description = "💲 🔨 Path of Exile app for price checking";
--- pkgs/exiled-exchange-2.nix
+++ pkgs/exiled-exchange-2.nix
@@ -3,18 +3,36 @@
   lib,
   fetchurl,
   appimageTools,
+  makeDesktopItem,
   ...
 }:
-
-#TODO add desktop entry etc
-
-appimageTools.wrapType2 rec {
+let
   pname = "exiled-exchange-2";
   version = "0.12.8";
+  icon = fetchurl {
+    url = "https://github.com/Kvan7/Exiled-Exchange-2/raw/master/renderer/public/images/jeweler.png";
+    sha256 = "sha256-30GXskIwOQ4m1a6dcm7MXVwlHsYvQ+SnavEGTKHqHUo=";
+  };
+
+  desktopItem = makeDesktopItem {
+    desktopName = "Exiled Exchange 2";
+    name = pname;
+    exec = pname;
+    icon = icon;
+  };
+in
+
+appimageTools.wrapType2 {
+  inherit pname version;
   src = fetchurl {
     url = "https://github.com/Kvan7/Exiled-Exchange-2/releases/download/v${version}/Exiled-Exchange-2-${version}.AppImage";
     hash = "sha256-hGUmwyhFsM+8XTrFCuaLVYAA85jwrKCftkQ/wlViRHI=";
   };
+
+  extraInstallCommands = ''
+    mkdir -p $out/share/applications
+    ln -s ${desktopItem}/share/applications/* $out/share/applications/
+  '';
 
   meta = with lib; {
     description = "Path of Exile 2 trading app for price checking";
